ed108db3a6bcf45ff302cf5876070a4ae9118548
William <william.gregorio.ny@gmail.com>
2025-07-06 06:13:22 -0400

    TypeScript configuration ts-loader, assuming path default is /dist

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,23 @@ module.exports = (_, argv) => {
     mode: mode(),
     devtool: devtool,
     entry: {
-      popup: "./src/popup/popup.js"
+      popup: "./src/popup/popup.ts"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
     },
     output: {
       filename: "[name].bundle.js",
       clean: true,
     }
-
   }
-}
+}   
